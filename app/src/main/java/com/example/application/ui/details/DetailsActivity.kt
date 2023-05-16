7ed5a9c63f0834cae1fce73dbaa445399d261b85
sina <cena.pakdel@gmail.com>
2023-05-16 07:18:31 +0330

    Save Recipe to Favorites

--- app/src/main/java/com/example/application/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/example/application/ui/details/DetailsActivity.kt
@@ -2,33 +2,33 @@ package com.example.application.ui.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
-import android.view.MenuInflater
 import android.view.MenuItem
-import androidx.appcompat.widget.SearchView
+import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
-import androidx.core.view.MenuHost
-import androidx.core.view.MenuProvider
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.navArgs
 import com.example.application.R
+import com.example.application.data.local.database.entities.FavoritesEntity
 import com.example.application.databinding.ActivityDetailsBinding
 import com.example.application.ui.details.adapter.PagerAdapter
+import com.example.application.ui.main.viewmodels.MainViewModel
 import com.example.application.utils.consts.Constants.Companion.INGREDIENTS
 import com.example.application.utils.consts.Constants.Companion.INSTRUCTIONS
 import com.example.application.utils.consts.Constants.Companion.OVERVIEW
 import com.example.application.utils.consts.Constants.Companion.RECIPE_BUNDLE_KEY
-import com.example.application.utils.view.onQueryTextSubmit
+import com.example.application.utils.view.makeSnack
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailsActivity : AppCompatActivity() {
     private var _binding: ActivityDetailsBinding? = null
     private val binding get() = _binding!!
     private lateinit var pagerAdapter: PagerAdapter
     private val args by navArgs<DetailsActivityArgs>()
     private val list = arrayListOf(OVERVIEW, INGREDIENTS, INSTRUCTIONS)
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,8 +49,9 @@ class DetailsActivity : AppCompatActivity() {
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_details,menu);return true
+        menuInflater.inflate(R.menu.menu_details, menu);return true
     }
+
     private fun implementViewPager(binding: ActivityDetailsBinding) {
         with(binding) {
             detailsViewPager.adapter = pagerAdapter
@@ -62,8 +63,23 @@ class DetailsActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) finish()
+        if (item.itemId == android.R.id.home) {
+            finish()
+        } else if (item.itemId == R.id.action_add_to_favorite) {
+            saveButtonClicked(item)
+        }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun saveButtonClicked(item: MenuItem) {
+        mainViewModel.insertFavoriteRecipes(FavoritesEntity(args.result, 0))
+        R.color.yellow.changeMenuItemColor(item)
+        makeSnack(getString(R.string.recipe_saved), binding.root)
+        Snackbar.make(binding.root, "make", Snackbar.LENGTH_LONG).setAction("OKAY") {}.show()
+    }
+
+    private fun Int.changeMenuItemColor(item: MenuItem) {
+        item.icon?.setTint(ContextCompat.getColor(this@DetailsActivity, this))
     }
 
     override fun onDestroy() {
