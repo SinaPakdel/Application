903831fbd42ff6ff5ab1fd2991cb359f91d0090c
sina <cena.pakdel@gmail.com>
2023-05-16 06:29:54 +0330

    Create details_menu

--- app/src/main/java/com/example/application/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/example/application/ui/details/DetailsActivity.kt
@@ -2,8 +2,16 @@ package com.example.application.ui.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.navArgs
 import com.example.application.R
 import com.example.application.databinding.ActivityDetailsBinding
@@ -12,6 +20,7 @@ import com.example.application.utils.consts.Constants.Companion.INGREDIENTS
 import com.example.application.utils.consts.Constants.Companion.INSTRUCTIONS
 import com.example.application.utils.consts.Constants.Companion.OVERVIEW
 import com.example.application.utils.consts.Constants.Companion.RECIPE_BUNDLE_KEY
+import com.example.application.utils.view.onQueryTextSubmit
 import com.google.android.material.tabs.TabLayoutMediator
 
 class DetailsActivity : AppCompatActivity() {
@@ -19,7 +28,8 @@ class DetailsActivity : AppCompatActivity() {
     private val binding get() = _binding!!
     private lateinit var pagerAdapter: PagerAdapter
     private val args by navArgs<DetailsActivityArgs>()
-    private val list= arrayListOf(OVERVIEW, INGREDIENTS, INSTRUCTIONS)
+    private val list = arrayListOf(OVERVIEW, INGREDIENTS, INSTRUCTIONS)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityDetailsBinding.inflate(layoutInflater)
@@ -34,8 +44,13 @@ class DetailsActivity : AppCompatActivity() {
 
         binding.materialToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_details,menu);return true
+    }
     private fun implementViewPager(binding: ActivityDetailsBinding) {
         with(binding) {
             detailsViewPager.adapter = pagerAdapter
