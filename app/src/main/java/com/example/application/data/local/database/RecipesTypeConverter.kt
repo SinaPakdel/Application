497226c6072f620302b843b82c4e24f442edbf05
sina <cena.pakdel@gmail.com>
2023-05-16 05:54:37 +0330

    Modify RecipesTypeConverter class

--- app/src/main/java/com/example/application/data/local/database/RecipesTypeConverter.kt
+++ app/src/main/java/com/example/application/data/local/database/RecipesTypeConverter.kt
@@ -2,6 +2,7 @@ package com.example.application.data.local.database
 
 import androidx.room.TypeConverter
 import com.example.application.data.models.FoodRecipes
+import com.example.application.data.models.FoodResult
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
@@ -17,6 +18,17 @@ class RecipesTypeConverter {
     @TypeConverter
     fun stringToFoodRecipe(data: String): FoodRecipes {
         val listType = object : TypeToken<FoodRecipes>() {}.type
+        return gson.fromJson(data, listType)
+    }
+
+    @TypeConverter
+    fun foodResultToString(result: FoodResult): String {
+        return gson.toJson(result)
+    }
+
+    @TypeConverter
+    fun stringToFoodResult(data: String): FoodResult {
+        val listType = object : TypeToken<FoodResult>() {}.type
         return gson.fromJson(data, listType)
     }
 }
