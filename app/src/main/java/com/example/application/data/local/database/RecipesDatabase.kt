333a3f3fae7da2cd3c090939d763445fe708944d
sina <cena.pakdel@gmail.com>
2023-05-16 06:21:14 +0330

    Modify RecipesDatabase class

--- app/src/main/java/com/example/application/data/local/database/RecipesDatabase.kt
+++ app/src/main/java/com/example/application/data/local/database/RecipesDatabase.kt
@@ -3,9 +3,10 @@ package com.example.application.data.local.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.application.data.local.database.entities.FavoritesEntity
 import com.example.application.data.local.database.entities.RecipeEntity
 
-@Database(entities = [RecipeEntity::class], version = 1, exportSchema = false)
+@Database(entities = [RecipeEntity::class,FavoritesEntity::class], version = 1, exportSchema = false)
 @TypeConverters(RecipesTypeConverter::class)
 abstract class RecipesDatabase :RoomDatabase(){
     abstract fun recipeDao(): RecipesDao
