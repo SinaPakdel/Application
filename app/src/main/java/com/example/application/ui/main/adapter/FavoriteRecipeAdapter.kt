df1a7e8232460350329e5a11c62ef99ebe7b4b8a
sina <cena.pakdel@gmail.com>
2023-05-16 09:50:48 +0330

    Create FavoriteRecipesAdapter class

---
+++ app/src/main/java/com/example/application/ui/main/adapter/FavoriteRecipeAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.application.ui.main.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.application.data.local.database.entities.FavoritesEntity
+import com.example.application.data.models.FoodRecipes
+import com.example.application.data.models.FoodResult
+import com.example.application.databinding.ItemFaovriteRecipesBinding
+import com.example.application.utils.diffutil.RecipesDiffUtil
+
+class FavoriteRecipeAdapter  : RecyclerView.Adapter<FavoriteRecipeAdapter.ViewHolder>() {
+    private var favoriteFoodResults = emptyList<FavoritesEntity>()
+
+    inner class ViewHolder(private val binding: ItemFaovriteRecipesBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(favoritesEntity: FavoritesEntity) {
+            binding.favoriteEntity = favoritesEntity
+            /**
+             * this will basically update all our views
+             */
+            binding.executePendingBindings()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
+        ViewHolder(ItemFaovriteRecipesBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun getItemCount(): Int = favoriteFoodResults.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentFavoriteFoodResult = favoriteFoodResults[position]
+        holder.bind(currentFavoriteFoodResult)
+    }
+
+    fun setData(favoritesEntityList: List<FavoritesEntity>) {
+        val diffUtil = RecipesDiffUtil(favoriteFoodResults, favoritesEntityList)
+        val calculateDiff = DiffUtil.calculateDiff(diffUtil)
+        favoriteFoodResults = favoritesEntityList
+        calculateDiff.dispatchUpdatesTo(this)
+    }
+}
