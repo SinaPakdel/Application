97ad094e51d73b18b506549987f869692cbdec03
sina <cena.pakdel@gmail.com>
2023-05-18 05:47:48 +0330

    Create favorite_recipes_menu
    Signed-off-by: sina <cena.pakdel@gmail.com>

--- app/src/main/java/com/example/application/ui/main/fragments/favorite/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/example/application/ui/main/fragments/favorite/FavoriteRecipesFragment.kt
@@ -3,16 +3,23 @@ package com.example.application.ui.main.fragments.favorite
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.application.R
 import com.example.application.databinding.FragmentFavoriteRecipesBinding
 import com.example.application.databinding.FragmentRecipesBinding
 import com.example.application.ui.main.adapter.FavoriteRecipeAdapter
 import com.example.application.ui.main.viewmodels.MainViewModel
+import com.example.application.utils.view.makeSnack
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,6 +29,8 @@ class FavoriteRecipesFragment : Fragment(R.layout.fragment_favorite_recipes) {
     private val binding get() = _binding!!
     private val fMainViewModel: MainViewModel by viewModels()
     private val favoriteRecipeAdapter by lazy { FavoriteRecipeAdapter(requireActivity(), fMainViewModel) }
+    private lateinit var menuHost: MenuHost
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -32,6 +41,14 @@ class FavoriteRecipesFragment : Fragment(R.layout.fragment_favorite_recipes) {
         binding.mainViewModel = fMainViewModel
         binding.favoriteAdapter = favoriteRecipeAdapter
 
+        menuHost = requireActivity()
+
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
             rvFavoriteRecipe.apply {
@@ -45,9 +62,23 @@ class FavoriteRecipesFragment : Fragment(R.layout.fragment_favorite_recipes) {
             Log.e(TAG, "onViewCreated: ${it.size}")
             favoriteRecipeAdapter.setData(it)
         }
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_favorite_recipe, menu)
+            }
 
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.actionDeleteAll -> {
+                        fMainViewModel.deleteAllFavoriteRecipes()
+                        makeSnack("Items Deleted",binding.root)
+                        ;true
+                    }
 
-        return binding.root
+                    else -> false
+                }
+            }
+        })
     }
 
 
