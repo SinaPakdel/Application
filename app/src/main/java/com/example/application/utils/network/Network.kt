0af6c29f588a73dc6d399cb1aea7fe63f5eea962
sina <cena.pakdel@gmail.com>
2023-05-20 15:31:00 +0330

    Add Motion Layout - OverviewFragment
    Signed-off-by: sina <cena.pakdel@gmail.com>

--- app/src/main/java/com/example/application/utils/network/Network.kt
+++
@@ -1,8 +0,0 @@
-package com.example.application.utils.network
-
-import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-
-
